4d82fa84365e70a76712de91c2136679eae054e4
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-02-18 22:31:42 +0100

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const Product= require('./models/product'); //connect table schema
+
+
+//connect databe mongoose to server
+mongoose.connect('mongodb+srv://honeyspring:g%72%65e%6El%61nd@cluster0-xoilo.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+  //implemented CORS to make sure the front end could safely make calls to your app.
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+app.use(bodyParser.json());//to extract the JSON object from the request
+
+app.post('/api/cart', (req, res, next) => {
+//created a new instance of your  Thing  model, passing it a JavaScript object containing all of the information it needs from the parsed request body
+  const thing = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  //save()method returns a promise, so in our  then()  block, we send back a success response, and in our  catch()  block, 
+  //we send back an error response with the error thrown by Mongoose.
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+
+});
+//get details of individual thing through id
+app.get('/api/cart/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+//update data of products
+app.put('/api/cart/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//delete route
+app.delete('/api/cart/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+//GET route to return all of the Things in the database
+app.use('/api/cart', (req, res, next) => {
+  Thing.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+
+module.exports = app;
